5817f5c42b390cec34973976e5b0f960b3be0389
raed-charrad <raed.charrad91@gmail.com>
2022-11-10 18:33:37 +0100

    add update

--- src/main/java/com/example/firstproject/web/RestOffers.java
+++ src/main/java/com/example/firstproject/web/RestOffers.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +39,11 @@ public class RestOffers {
     @DeleteMapping("/{id}")
     public void deleteoffre(@PathVariable Long id) {
         offreRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Offre updateoffre(@PathVariable Long id, @RequestBody Offre offre) {
+        offre.setId(id);
+        return offreRepository.save(offre);
     }
 }
